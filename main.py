36a46aa49dadc6e24f22eaf5a68b71438910bb89
leeeunbin219 <leeeunbin219@gmail.com>
2026-08-09 20:10:52 +0900

    feat: QuizGame 클래스 구현

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ class Quiz:
     
     def check_answer(self, user_answer):
         return self.answer == user_answer  # 정답 확인
+
+class QuizGame:
+    def __init__(self):
+        self.score = 0          # 점수
+        self.quizzes = []       # 퀴즈 목록
+    
+    def add_quiz(self, quiz):
+        self.quizzes.append(quiz)   # 퀴즈 추가
+    
+    def run(self):
+        print("=== 롤 챔피언 퀴즈 게임 ===")
+        for quiz in self.quizzes:
+            print(quiz.question)    # 문제 출력
