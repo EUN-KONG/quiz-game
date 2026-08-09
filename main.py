ab0a89a05f467a435ea6a35621dc624113fb0c80
leeeunbin219 <leeeunbin219@gmail.com>
2026-08-09 20:08:55 +0900

    feat: Quiz 클래스 구현

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # 롤 챔피언 퀴즈 게임
 # main.py
+class Quiz:
+    def __init__(self, question, answer, choices):
+        self.question = question    # 문제
+        self.answer = answer        # 정답
+        self.choices = choices      # 보기 리스트
+    
+    def check_answer(self, user_answer):
+        return self.answer == user_answer  # 정답 확인
